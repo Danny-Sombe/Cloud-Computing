16a97bebc719b47515a8f268c44bd6ae7ca79b7b
SonnLogix <microsoftedge862@gmail.com>
2025-11-06 18:32:36 +1000

    module-03 added

---
+++ Cloud-SAAS/module-03/terraform.tf
@@ -0,0 +1,22 @@
+# Add values
+# Find the Ubuntu server 22.04 AMI for your region at this URL
+# https://cloud-images.ubuntu.com/locator/ec2/
+imageid                = "ami-0279a86684f669718"
+# Use t2.micro for the AWS Free Tier
+instance-type          = "t2.micro"
+key-name               = "sonnlogix-key"
+vpc_security_group_ids = "sg-0c38a5b78e9bb4b79"
+tag-name               = "module-03"
+raw-s3                 = 
+finished-s3            = 
+user-sns-topic         = 
+elb-name               = 
+tg-name                = 
+asg-name               = 
+desired                = 3
+min                    = 2
+max                    = 5
+number-of-azs          = 
+region                 = 
+raw-s3-bucket          = 
+finished-s3-bucket     = 
