e66c42be05558e66de432fd160be27f8de3e8c46
Danny-Sombe <solutiontech954@gmail.com>
2026-02-10 14:48:36 +1000

    lets see now

--- Cloud-SAAS/module-05/Final-Projects/M5-assessment-templates-and-code/main.tf
+++ Cloud-SAAS/module-05/Final-Projects/M5-assessment-templates-and-code/main.tf
@@ -379,7 +379,7 @@ output "url" {
 # Create a new ALB Target Group attachment
 # Create a new ALB Target Group attachment
 resource "aws_autoscaling_attachment" "alb" {
-  autoscaling_group_name = aws_autoscaling_group.alb.id
+  autoscaling_group_name = aws_autoscaling_group.asg.id
   lb_target_group_arn    = aws_lb_target_group.alb-lb-tg.arn
 }
 
