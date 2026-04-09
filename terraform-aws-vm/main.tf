87a7f777924ffab7d16db218c2ebb96be6ffad98
Danny-Sombe <solutiontech954@gmail.com>
2026-04-10 08:47:46 +1000

    added main to ubuntu

--- terraform-aws-vm/main.tf
+++ terraform-aws-vm/main.tf
@@ -1,23 +1,28 @@
-provider "aws" {
-  region = "ap-southeast-2" # Sydney (closest to PNG)
-}
+# EC2 Creation
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0" # change based on region
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
 
-# ----------------------
-# VPC
-# ----------------------
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = "your-key-name"
 
   tags = {
-    Name = "terraform-vpc"
+    Name = "web-server"
   }
 }
 
-# ----------------------
-# Public Subnet
-# ----------------------
+# VPC + Subnets
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
@@ -26,11 +31,8 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-# ----------------------
-# Private Subnet
-# ----------------------
 resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
@@ -38,53 +40,46 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-# ----------------------
 # Internet Gateway
-# ----------------------
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "main-igw"
   }
 }
 
-# ----------------------
-# Route Table (Public)
-# ----------------------
+# Route Table
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "public-rt"
+    Name = "public-route-table"
   }
 }
 
-# Associate Route Table
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# ----------------------
 # Security Group
-# ----------------------
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.main_vpc.id
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
+  vpc_id = aws_vpc.main.id
 
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # You can restrict later
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -103,29 +98,38 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# ----------------------
-# EC2 Instance
-# ----------------------
-resource "aws_instance" "web_server" {
-  ami           = "ami-084fe2982f7b555c8" # Ubuntu 20.04 (Sydney region)
-  instance_type = "t2.micro"
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name   = "rds-sg"
+  vpc_id = aws_vpc.main.id
 
-  subnet_id = aws_subnet.public_subnet.id
+  ingress {
+    description     = "MySQL from EC2"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
 
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  associate_public_ip_address = true
-
-  key_name = "demo" # IMPORTANT: change this
-
-  tags = {
-    Name = "terraform-ec2"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# ----------------------
-# Output
-# ----------------------
-output "public_ip" {
-  value = aws_instance.web_server.public_ip
+# Elastic IP
+resource "aws_eip" "ec2_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "ec2-eip"
+  }
+}
+
+# Associate EIP with EC2
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.ec2_eip.id
 }
